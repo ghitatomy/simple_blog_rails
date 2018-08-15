e683609b66d7e3f1400b8ff2469be38a19928374
ghitatomy <ghita@luminos.software>
2018-08-15 22:13:57 +0300

    Add comment model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index', as: 'home'
   get 'about' => 'pages#about', as: 'about'
-  resources :posts
+  resources :posts do # posts/1/comments
+    resources :comments
+  end
 end
